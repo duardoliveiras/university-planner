5941ec2059f4a7b6b206e6f7ecf9887564abeacf
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-10-14 10:07:52 +0100

    update changes in db coeher

--- functions/dbStudents.cpp
+++ functions/dbStudents.cpp
@@ -7,9 +7,14 @@ void printDB(vector<int> vecDB);
 
 
 void dbStudents(int submenu, int order) {
+  cout << "This functions is not ready yet." << endl;
   // It will not be a vector of integers but of class student or uc.
   vector <int> vecDB; 
 
+  // put information that user choose in vecDB
+
+
+  // print information in vecDB
   printDB(vecDB);
 
 }
--- functions/dbUcs.cpp
+++ functions/dbUcs.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void printDB(vector<int> vecDB);
+
 void dbUcs(int submenu, int order) {
   cout << "This functions is not ready yet." << endl;
+
+    // It will not be a vector of integers but of class student or uc.
+  vector <int> vecDB; 
+
+  // put information that user choose in vecDB
+
+
+  // print information in vecDB
+  printDB(vecDB);
 }
