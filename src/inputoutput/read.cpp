158757c97d7646e607b6a3cf011f825f0b7ccc17
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-10-22 21:54:31 +0100

    make it simpler

---
+++ src/inputoutput/read.cpp
@@ -0,0 +1,142 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../classes/class.h"
+
+void workingMessage();
+void errorMessageFile();
+void errorMessageLine(std::string);
+
+std::vector<myClass> readAllClasses() { // FUNCTION FINISH
+  std::vector<myClass> data;
+
+  // std::ifstream classesdata("schedule/classes.csv");
+
+  // // check if open
+  // if (!classesdata.is_open()) {
+  //   errorMessageFile();
+  // }
+
+  // std::string line;
+  // bool header = true;
+
+  // while (std::getline(classesdata, line)) {
+  //   if (header) {
+  //     header = false;
+  //     continue;
+  //   }
+
+  //   std::stringstream ss(line);
+  //   std::string token;
+  //   std::vector<std::string> tokens;
+
+  //   while (std::getline(ss, token, ',')) {
+  //     tokens.push_back(token);
+  //   }
+
+  //   // Check if file has at least 7 parameters:
+  //   // ClassCode,UcCode,Weekday,StartHour,Duration,Type,Date
+  //   if (tokens.size() < 7) {
+  //     errorMessageLine(line);
+  //   }
+
+  //   // Store information
+  //   std::string classCode = tokens[0];
+  //   std::string ucCode = tokens[1];
+
+  //   // Parse Slot data
+  //   std::string weekday = tokens[2];
+  //   float startHour = std::stof(tokens[3]);
+  //   float duration = std::stof(tokens[4]);
+  //   std::string type = tokens[5];
+  //   std::string date = tokens[6];
+
+  //   // Create Slot object and add it to the vector
+  //   Slot slot{weekday, startHour, duration, type};
+  //   std::vector<Slot> slots = {slot};
+
+  //   myClass myclass(classCode, ucCode, slots);
+  //   data.push_back(myclass);
+  // }
+
+  // classesdata.close();
+  return data;
+}
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../classes/student.h"
+
+void workingMessage();
+void errorMessageFile();
+void errorMessageLine(std::string);
+
+std::vector<myStudent> readAllStudents() { // FUNCTION FINISH
+  std::vector<myStudent> data;
+
+  // std::ifstream studentsdata("schedule/students_classes.csv");
+
+  // // check if open
+  // if (!studentsdata.is_open()) {
+  //   errorMessageFile();
+  // }
+
+  // std::string line;
+  // bool header = true;
+
+  // while (std::getline(studentsdata, line)) {
+
+  //   if (header) {
+  //     header = false;
+  //     continue;
+  //   }
+
+  //   std::stringstream ss(line);
+  //   std::string token;
+  //   std::vector<std::string> tokens;
+
+  //   while (std::getline(ss, token, ',')) {
+  //     tokens.push_back(token);
+  //   }
+
+  //   // Check if file have 4 parameters:
+  //   // StudentCode,StudentName,UcCode,ClassCode
+  //   if (tokens.size() < 4) {
+  //     errorMessageLine(line);
+  //   }
+
+  //   int studentCode = std::stoi(tokens[0]);
+  //   std::string studentName = tokens[1];
+  //   std::vector<std::string> ucCode = {tokens[2]};
+  //   std::vector<std::string> classCode = {tokens[3]};
+
+  //   myStudent mystudent(studentCode, studentName, ucCode, classCode);
+  //   data.push_back(mystudent);
+  // }
+
+  // studentsdata.close();
+  return data;
+}
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "../classes/uc.h"
+
+void workingMessage();
+
+std::vector<myUc> readAllUcs() {
+  std::vector<myUc> data;
+
+  workingMessage();
+
+  // READ FILE AND STORE INFO IN DATA
+
+  return data;
+}
